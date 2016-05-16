c27423f7c3d88b2943bd5f7128050fa9b53ca5ee
eddy <eddy@concordess.com>
2016-05-17 01:09:44 +0800

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions shall enable cached operation of matrix inverse results
+## to avoid unnecessary matrix inverse calculation for performance improvements
 
-## Write a short comment describing this function
 
+## makeCacheMatrix function is to return a list that provide the necessary operation to set/get data and cached results
+## Input to the function is the matrix to be cached.
+## Output is the list that provide the necessary operation to set/get data and cached results
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(i) inverse <<- i
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve function operates using makeCacheMatrix function to set/get cached matrix inverse results
+## Input to the function is the list created by makeCacheMatrix
+## Output is the matrix inverse result of the input matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return (inverse)
+        }
+        
+        data<- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
+
+
+# Simple Test
+matrix1 = matrix(c(1,2,3,4), 2,2)
+cache1 = makeCacheMatrix(matrix1)
+cacheSolve(cache1)
+
+
